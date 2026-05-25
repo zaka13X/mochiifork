1369e07489f8c579c492c8e3c8e2c38716814e3e
python-mxnt <verxateam@gmail.com>
2026-05-25 20:26:55 +0100

    fuck vercel

--- api/chat.js
+++ api/chat.js
@@ -14,7 +14,7 @@ export default async function handler(req) {
             headers: { 'Content-Type': 'application/json' }
         });
     }
-
+// fuck
     const origin = req.headers.get('origin') || '';
     const referer = req.headers.get('referer') || '';
     const allowed = ['mochiii.vercel.app', 'localhost'];
